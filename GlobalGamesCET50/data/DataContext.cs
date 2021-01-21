054b985bcd65e2051ccd21ea85787493bc4fd084
Catarina Matos <Catarina.Barbosa.Matos@formandos.cinel.pt>
2021-01-21 18:00:04 +0000

    ggservicos done

---
+++ GlobalGamesCET50/data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using GlobalGamesCET50.data.entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGamesCET50.data
+{
+    public class DataContext : DbContext 
+    {
+
+        public DbSet<servicos> Servicos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ GlobalGamesCET50/data/entities/Servicos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGamesCET50.data.entities
+{
+    public class servicos
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mensagem { get; set; }
+
+    }
+}
